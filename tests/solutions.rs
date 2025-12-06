06722f864debc4c180760200961011157ccc9c68
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2025-12-06 19:01:31 +0100

    feat: day6 part1

--- tests/solutions.rs
+++ tests/solutions.rs
@@ -24,3 +24,4 @@ solution_day!(day2, "38437576669", "49046150754");
 solution_day!(day3, "17343", "172664333119298");
 solution_day!(day4, "1419", "8739");
 solution_day!(day5, "888", "344378119285354");
+solution_day!(day6, "4309240495780", "0"); // TODO part 2
